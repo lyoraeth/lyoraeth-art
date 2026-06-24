dd23a88e1216da991bcfa5a98db5810ab78f452e
lyoraeth <lyoraeth@gmail.com>
2026-06-24 12:05:58 +0300

    feat(studio): add projectId/dataset to sanity.cli.ts for deploy

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,6 +1,10 @@
 import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
+  api: {
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID ?? 'smml2m66',
+    dataset:   process.env.SANITY_STUDIO_DATASET    ?? 'production',
+  },
   server: {
     hostname: process.env.STUDIO_HOST ?? 'localhost',
     port:     parseInt(process.env.STUDIO_PORT ?? '3333'),
